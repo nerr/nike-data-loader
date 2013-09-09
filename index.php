4d9639a815051092928572ab741cf3368b3b8e56
@nerrsoft <leon@nerrsoft.com>
2013-09-09 10:41:20 +0800

    add google analytics code

--- index.php
+++ index.php
@@ -11,12 +11,14 @@
 
     $activites = $n->activities($num);
 
+    var_dump($activites);
+
     foreach($activites as $key => $value)
     {
         $run['id'] = $value->activityId;
         $run['date'] = date('Y-m-d', strtotime($value->startTimeUtc));
         $run['duration'] = gmdate("H:i:s", $value->metrics->duration/1000);
-        $run['distance'] = number_format($value->metrics->distance, 1).'km';
+        $run['distance'] = number_format($value->metrics->distance, 1);
         $run['pace'] = gmdate("i's''", $value->metrics->duration/1000/$value->metrics->distance);
         $run['fuel'] = $value->metrics->fuel;
 
@@ -24,8 +26,18 @@
     }
 
     $all = $n->allTime();
-    $runs['summary']['alltimedistance'] = number_format($all->lifetimeTotals->distance, 1).'km';
-    $runs['summary']['thismounthdistance'] = number_format($all->homepageStats->totalDistance, 1).'km';
+    $runs['summary']['alltimedistance'] = number_format($all->lifetimeTotals->distance, 1);
+    $runs['summary']['thismounthdistance'] = number_format($all->homepageStats->totalDistance, 1);
 
     echo $_GET['callback'].'('.json_encode($runs).')';
 ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-28003762-3', 'nerrsoft.com');
+  ga('send', 'pageview');
+
+</script>
